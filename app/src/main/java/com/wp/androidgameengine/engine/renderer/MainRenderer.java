7ed08f00812b6d2ae1b4177b01f43c94ee8c75d5
Miskov <msz93@o2.pl>
2016-04-25 21:48:03 +0200

    OpenGL initial commit

---
+++ app/src/main/java/com/wp/androidgameengine/engine/renderer/MainRenderer.java
@@ -0,0 +1,7 @@
+package com.wp.androidgameengine.engine.renderer;
+
+/**
+ * Created by msz93 on 25.04.2016.
+ */
+public class MainRenderer {
+}
---
+++ app/src/main/java/com/wp/androidgameengine/engine/surface/MainSurfaceView.java
@@ -0,0 +1,7 @@
+package com.wp.androidgameengine.engine.surface;
+
+/**
+ * Created by msz93 on 25.04.2016.
+ */
+public class MainSurfaceView {
+}
